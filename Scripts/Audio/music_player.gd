9ccf533f66062d502e16ce1b9e69ba809938bff0
imkimdol <kimdol@proton.me>
2025-08-22 16:30:09 -0700

    Make pushing functions static

--- Scripts/Audio/music_player.gd
+++ Scripts/Audio/music_player.gd
@@ -9,6 +9,13 @@ static var queue: Array[AudioStream] = []
 
 # *** Public ***
 
+# Loads the given audio file into memory and adds it to the queue.
+# Does not load anything if the queue is full.
+static func push_queue(file_location: String):
+	if queue.size() >= queue_max_size:
+		return
+	queue.push_back(_load_audio_file(file_location))
+
 # Plays the given file immediately.
 # Clears the queue.
 func play_now(file_location: String):
@@ -17,13 +24,6 @@ func play_now(file_location: String):
 	current = _load_audio_file(file_location)
 	stream = current
 	play()
-
-# Loads the given audio file into memory and adds it to the queue.
-# Does not load anything if the queue is full.
-func push_queue(file_location: String):
-	if queue.size() >= queue_max_size:
-		return
-	queue.push_back(_load_audio_file(file_location))
 
 # Plays the next song in the queue.
 # Does nothing if the queue is empty.
@@ -38,13 +38,13 @@ func play_next():
 
 # *** Private ***
 
-func _load_audio_file(file_location: String) -> AudioStream:
+static func _load_audio_file(file_location: String) -> AudioStream:
 	assert(ResourceLoader.exists(file_location))
 	var loaded_res = load(file_location)
 	assert(loaded_res is AudioStream)
 	return loaded_res
 
-func _push_history():
+static func _push_history():
 	history.push_back(current)
 	current = null
 	if history.size() > history_max_size:
