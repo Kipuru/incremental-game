e0e2da2afe35bc8d4829329bca856b66302a2ffe
imkimdol <kimdol@proton.me>
2025-08-27 07:20:00 -0700

    Make bubbles merge on collision with same tier

--- Scripts/Gameplay/bubble.gd
+++ Scripts/Gameplay/bubble.gd
@@ -1,10 +1,16 @@
-extends CharacterBody2D
+class_name Bubble extends CharacterBody2D
 
+const bubble_scene = preload("uid://cqegup4gnccd3")
 const droplets_scene = preload("uid://dtcbmrnwmh1m6")
 
+@onready var refraction: Sprite2D = %Refraction
+
+var tier := 0
 var touching_mouse := false
 
 func _ready() -> void:
+	assert(bubble_scene is PackedScene)
+	assert(bubble_scene.can_instantiate())
 	assert(droplets_scene is PackedScene)
 	assert(droplets_scene.can_instantiate())
 	
@@ -13,9 +19,7 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	var collision = move_and_collide(velocity * delta)
 	if collision:
-		var bounce = velocity.bounce(collision.get_normal())
-		position += bounce / 128
-		velocity = bounce
+		handle_collision(collision)
 
 func _input(event: InputEvent):
 	if touching_mouse and event is InputEventMouseButton:
@@ -28,6 +32,41 @@ func _mouse_enter() -> void:
 
 func _mouse_exit() -> void:
 	touching_mouse = false
+
+func handle_collision(collision: KinematicCollision2D) -> void:
+	var collider = collision.get_collider()
+	if collider is not Bubble:
+		bounce(collision)
+		return
+	
+	var bubble = collider as Bubble
+	if bubble.tier != tier:
+		bounce(collision)
+		return
+	
+	handle_bubble_collision_same_tier(bubble)
+
+# Bubble with higher coordinate will spawn new bubble
+func handle_bubble_collision_same_tier(collided_with: Bubble) -> void:
+	if position.x < collided_with.position.x:
+		queue_free()
+		return
+	elif position.x == collided_with.position.x and position.y < collided_with.position.y:
+		queue_free()
+		return
+	
+	var bubble_instance = bubble_scene.instantiate()
+	assert(bubble_instance is Bubble)
+	bubble_instance.position = position.lerp(collided_with.position, 0.5) # avg position of both bubbles
+	bubble_instance.velocity = velocity.lerp(collided_with.velocity, 0.5) # avg velocity of both bubbles
+	get_parent().add_child(bubble_instance)
+	bubble_instance.tier = tier + 1
+	queue_free()
+
+func bounce(collision: KinematicCollision2D) -> void:
+	var bounced = velocity.bounce(collision.get_normal())
+	position += bounced / 128
+	velocity = bounced
 
 func handle_pop() -> void:
 	var droplets_instance = droplets_scene.instantiate()
