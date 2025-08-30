aec134f1899a46c1a61bff7bfe4881002ecb2865
imkimdol <kimdol@proton.me>
2025-08-29 19:20:23 -0700

    Remove old gameplay (scenes preserved)

--- Scripts/UI/taskbar.gd
+++ Scripts/UI/taskbar.gd
@@ -1,14 +1,9 @@
 extends PanelContainer
 
-@export var gameplay_window: Node2D
 @export var music_window: Node2D
 
 func _ready() -> void:
-	assert(gameplay_window)
 	assert(music_window)
-
-func _on_gameplay_button_pressed() -> void:
-	gameplay_window.set_visible(true)
 
 func _on_music_button_pressed() -> void:
 	music_window.visible = not music_window.visible
