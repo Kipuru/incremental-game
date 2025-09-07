d5e9e68bbe5690e8a9e56d33fee878e2b783bf3d
imkimdol <contact@harperkim.ca>
2025-09-07 11:03:48 -0700

    Bug fix

--- Scripts/UI/Music/SongsDisplay/songs_display.gd
+++ Scripts/UI/Music/SongsDisplay/songs_display.gd
@@ -4,25 +4,12 @@ const song_item_scene: PackedScene = preload("uid://crukf30s30kkm")
 
 # !!!!! These are temp, better solution should be implemented later using a json file or something !!!!!
 const song_folder = "res://Assets/Demo Songs/"
-func get_files_in_folder(path: String) -> Array:
-	var files: Array
-	var dir = DirAccess.open(path)
-
-	if dir:
-		dir.list_dir_begin()
-		var file_name = dir.get_next()
-		while file_name != "":
-			if not dir.current_is_dir() and file_name.ends_with(".mp3"):
-				files.append(file_name)
-			file_name = dir.get_next()
-		dir.list_dir_end()
-	return files
 func load_songs():
-	var files = get_files_in_folder(song_folder)
+	var files = ResourceLoader.list_directory(song_folder)
 	for i in range(files.size()):
 		var instance = instantiate_song_item()
 		instance.song_name = files[i].split(".")[0]
-		instance.location = "res://Assets/Demo Songs/" + files[i]
+		instance.location = song_folder + files[i]
 		instance.play_now.connect(play_now)
 		instance.enqueue.connect(enqueue)
 		container.add_child(instance)
