524e3d4f76a6e4a3625050f258fe626ec995a759
imkimdol <contact@harperkim.ca>
2025-09-14 18:40:07 -0700

    Make bubble movement a bit nicer

--- Scripts/Gameplay/bubble.gd
+++ Scripts/Gameplay/bubble.gd
@@ -66,7 +66,7 @@ func merge_bubbles(collided_with: Bubble) -> void:
 func bounce(collision: KinematicCollision2D) -> void:
 	var bounced = velocity.bounce(collision.get_normal())
 	position += bounced / 64
-	velocity = bounced
+	velocity = bounced.ceil()
 
 func handle_pop() -> void:
 	#var droplets_instance = droplets_scene.instantiate()
