c75ecafbd932ab337eaea3694a4d2f27a99f3635
imkimdol <contact@harperkim.ca>
2025-09-16 19:10:36 -0700

    Add cooldown to bubble timer

--- Scripts/Gameplay/bubble_spawner.gd
+++ Scripts/Gameplay/bubble_spawner.gd
@@ -1,12 +1,18 @@
 class_name BubbleSpawner extends Area2D
 
 const bubble_scene = preload("uid://cqegup4gnccd3")
+const base_spawn_cooldown_duration = 5.
 
 @export var bubble_container: Node2D
 static var touching_mouse := false
+var cooldown = 0.
 
 func _ready() -> void:
 	assert(bubble_scene.can_instantiate())
+
+func _process(delta: float) -> void:
+	tick_timer(delta)
+	rotation = 2 * PI * cooldown / base_spawn_cooldown_duration # temp visual for timer
 
 func _mouse_enter() -> void:
 	touching_mouse = true
@@ -17,10 +23,19 @@ func _mouse_exit() -> void:
 func _input(event: InputEvent):
 	if touching_mouse and event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			spawn_bubble()
+			if cooldown == 0.:
+				spawn_bubble()
 
 func spawn_bubble():
 	var bubble_instance = bubble_scene.instantiate()
 	assert(bubble_instance is PhysicsBody2D)
 	bubble_instance.global_position = global_position
 	bubble_container.add_child(bubble_instance)
+	cooldown = base_spawn_cooldown_duration
+
+func tick_timer(delta: float) -> void:
+	if cooldown == 0.: 
+		return
+	cooldown -= delta
+	if cooldown < 0.:
+		cooldown = 0.
