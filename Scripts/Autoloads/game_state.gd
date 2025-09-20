d0fe1718a48b5276c1e04bf2d24f644ff98097be
imkimdol <contact@harperkim.ca>
2025-09-20 10:09:25 -0700

    Add upgrade vars to GameState

--- Scripts/Autoloads/game_state.gd
+++ Scripts/Autoloads/game_state.gd
@@ -1,8 +1,31 @@
 extends Node
 
 signal bubbles_updated(bubbles: int)
-
 var bubbles := 0:
 	set(value):
 		bubbles = value
 		bubbles_updated.emit(value)
+
+signal stage_updated(tier: int)
+var stage := 0:
+	set(value):
+		stage = value
+		stage_updated.emit(value)
+
+signal bubble_spawner_cooldown_tier_updated(tier: int)
+var bubble_spawner_cooldown_tier := 0:
+	set(value):
+		bubble_spawner_cooldown_tier = value
+		bubble_spawner_cooldown_tier_updated.emit(value)
+
+signal bubble_decay_tier_updated(tier: int)
+var bubble_decay_tier := 0:
+	set(value):
+		bubble_decay_tier = value
+		bubble_decay_tier_updated.emit(value)
+
+signal bubble_click_damage_tier_updated(tier: int)
+var bubble_click_damage_tier := 0:
+	set(value):
+		bubble_click_damage_tier = value
+		bubble_click_damage_tier_updated.emit(value)
