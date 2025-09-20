3cb22f17e1c4931d468d8e5bf8816d1c3aa62f91
imkimdol <contact@harperkim.ca>
2025-09-20 10:27:11 -0700

    Add debug display for GameState

--- Scripts/Autoloads/debug_helper.gd
+++ Scripts/Autoloads/debug_helper.gd
@@ -1,10 +1,17 @@
 extends Node
 
-var fps_label: Label = null
+var debug_label: Label = null
+var game_state_var_names: Array[String] = []
+
+func _ready() -> void:
+	var script := GameState.get_script() as GDScript
+	for variable in script.get_script_property_list():
+		game_state_var_names.append(variable.name)
+		print(variable.name)
 
 func _process(_delta: float) -> void:
-	if fps_label != null:
-		fps_label.text = "FPS: " + str(Engine.get_frames_per_second())
+	if debug_label != null:
+		edit_debug()
 
 func _input(event):
 	if not (event is InputEventKey and event.pressed):
@@ -18,11 +25,29 @@ func _input(event):
 			# set window mode to fullscreen
 			DisplayServer.window_set_mode(DisplayServer.WindowMode.WINDOW_MODE_FULLSCREEN)
 		KEY_F12:
-			# show FPS on screen
-			var canvas_layer = CanvasLayer.new()
-			get_tree().root.add_child(canvas_layer)
-			var label = Label.new()
-			label.add_theme_font_size_override("font_size", 50)
-			canvas_layer.add_child(label)
-			canvas_layer.layer = 999
-			fps_label = label
+			# show debug
+			if debug_label == null:
+				add_debug()
+
+func add_debug():
+	var canvas_layer = CanvasLayer.new()
+	get_tree().root.add_child(canvas_layer)
+	var label = Label.new()
+	label.add_theme_font_size_override("font_size", 12)
+	canvas_layer.add_child(label)
+	canvas_layer.layer = 999
+	debug_label = label
+
+func edit_debug():
+	var text = ""
+	text += "FPS: " + str(Engine.get_frames_per_second())
+	text += "\n"
+	
+	for var_name in game_state_var_names:
+		var value = GameState.get(var_name)
+		if value == null:
+			continue
+		text += var_name + ":  " + str(value)
+		text += "\n"
+	
+	debug_label.text = text
