8974d8a1c3583b6e87e119b405096886aa5889c7
imkimdol <contact@harperkim.ca>
2025-11-19 20:21:41 -0800

    Wire up launcher

--- Game/UI/Overlay/launcher.gd
+++ Game/UI/Overlay/launcher.gd
@@ -1,20 +1,18 @@
 class_name Launcher extends PanelContainer
 
-@export var windows: Node2D
-@export var taskbar: Taskbar
+@export var windows: GameWindowManager
 
 func _ready() -> void:
-	assert(windows and taskbar)
+	assert(windows)
 	visible = false
 
 func _on_store_pressed() -> void:
-	pass
+	windows.add_or_show_window(GameWindowManager.STORE_WINDOW_INFO)
+	visible = false
 
 func _on_music_player_pressed() -> void:
-	pass
+	windows.add_or_show_window(GameWindowManager.MUSIC_PLAYER_WINDOW_INFO)
+	visible = false
 
 func _on_settings_pressed() -> void:
-	pass
-
-func _add_or_show_window():
 	pass
--- Game/UI/Overlay/taskbar.gd
+++ Game/UI/Overlay/taskbar.gd
@@ -2,26 +2,33 @@ class_name Taskbar extends PanelContainer
 
 const item_scene = preload("uid://bpup7sctor3jd")
 
-@export var windows: Array[GameWindow]
 @export var launcher: Launcher
 
 @onready var items: Control = %Items
 @onready var launcher_button: TextureButton = %LauncherButton
 
 func _ready() -> void:
-	assert(windows and launcher)
+	assert(launcher)
 	assert(item_scene.can_instantiate())
 	
 	launcher_button.pressed.connect(
 		func():
 			launcher.visible = not launcher.visible
 	)
+
+func add_item(window: GameWindow):
+	var item = item_scene.instantiate()
+	assert(item is TaskbarItem)
 	
-	for window in windows:
-		var item = item_scene.instantiate()
-		assert(item is TaskbarItem)
-		
-		item.window = window
-		item.init()
-		
-		items.add_child(item)
+	item.window = window
+	item.init()
+	
+	items.add_child(item)
+
+# iteration is fine here bc we won't have many windows
+func remove_item(window: GameWindow):
+	for child in items.get_children():
+		assert(child is TaskbarItem)
+		var item = child as TaskbarItem
+		if item.window == window:
+			item.queue_free()
