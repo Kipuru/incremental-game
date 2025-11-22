e56e5b3ebd75702147e28358a363b9753ba71eba
fp3 <97940804+kupa-fp3@users.noreply.github.com>
2025-11-22 03:47:56 -0800

    shortened volume slider

--- Game/UI/Music/vol_slider.gd
+++ Game/UI/Music/vol_slider.gd
@@ -9,8 +9,10 @@ var busIndex: int
 func _ready() -> void:
 	busIndex = AudioServer.get_bus_index(BusName)
 	value_changed.connect(_onvalchanged)
-	value = db_to_linear(AudioServer.get_bus_volume_db(busIndex))
-	
+	#value = db_to_linear(AudioServer.get_bus_volume_db(busIndex))
+	#setting default vol to 50%
+	value = 0.5	# 50%
+	AudioServer.set_bus_volume_db(busIndex, linear_to_db(value))
 	
 	
 func _onvalchanged(value:float) -> void:
