13993c6cadd94fe67da1cd54930ffc723b5949b9
fp3 <97940804+kupa-fp3@users.noreply.github.com>
2025-12-24 23:58:24 -0800

    Refixed Issues

--- Game/UI/Music/vol_slider.gd
+++ Game/UI/Music/vol_slider.gd
@@ -1,17 +1,10 @@
 extends HSlider
 
 @export var audioPlayer: AudioStreamPlayer
-#var BusName: String
-
-#var busIndex: int
 
 
 func _ready() -> void:
-	#busIndex = AudioServer.get_bus_index(BusName)
-	#value_changed.connect(_onvalchanged)
-	#value = db_to_linear(AudioServer.get_bus_volume_db(busIndex))
-	#setting default vol to 50%
-	#AudioServer.set_bus_volume_db(busIndex, linear_to_db(value))
+
 	min_value = 0.0
 	max_value = 1.0
 	step = 0.05
@@ -25,4 +18,3 @@ func _ready() -> void:
 	
 func _onvalchanged(value:float) -> void:
 	audioPlayer.volume_db = linear_to_db(value)
-	#AudioServer.set_bus_volume_db(busIndex,linear_to_db(value))
